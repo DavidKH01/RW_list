3b127de8a841e8bcca0d34a8caf5bd528155efdc
Danny <david_khart@outlook.com>
2023-09-14 18:04:40 -0400

    first commit

---
+++ rw_backend/rw_dbconnect.py
@@ -0,0 +1,15 @@
+#code copied from mongobd connect
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://danny:0@cluster0.hgtihbs.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
