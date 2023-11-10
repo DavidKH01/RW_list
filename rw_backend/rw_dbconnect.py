2b80a2c0c76dd5aa539ea8620d26e039173ce001
Danny <david_khart@outlook.com>
2023-11-10 13:50:50 -0500

    add names to tile

--- rw_backend/rw_dbconnect.py
+++ rw_backend/rw_dbconnect.py
@@ -24,30 +24,3 @@ def connect():
         print(e)
 
 
-def wlist():
-
-    #.list_database_names are built in mongo feature
-    mongocl = connect()
-    #print(mongocl.list_database_names())
-    mydb = mongocl["sample_list"]
-    #print(mydb.list_collection_names())
-    mywlist = mydb["watch_list"]
-
-
-    # count number of items in collections 
-    count = mywlist.count_documents({})
-    
-       
-    for x in range(count):
-        # need to remove _id key as it turns item into an non iterable object
-        # print(mywlist.find()[x])
-        lit = mywlist.find()[x]
-        del lit['_id']
-        print(lit)
-  
-
-
-
-
-
-wlist()
--- rw_backend/rw_dbqueires.py
+++ rw_backend/rw_dbqueires.py
@@ -1,27 +1,34 @@
-import rw_dbconnect as rwc
-rwlist = rwc.wlist()
+import rw_dbconnect as cn
+
+#.list_database_names are built in mongo feature
+mongocl = cn.connect()
+#print(mongocl.list_database_names())
+mydb = mongocl["sample_list"]
+#print(mydb.list_collection_names())
+mywlist = mydb["watch_list"]
+
+
+def full_list():
+    # count number of items in collections 
+    count = mywlist.count_documents({})
+    f_list=[]
+    for x in range(count):
+        # need to remove _id key as it turns item into an non iterable object
+        # print(mywlist.find()[x])
+        lit = mywlist.find()[x]
+        del lit['_id']
+        f_list.append(lit)
+        print(lit)
+    return f_list
+
+def get_names():
+    #db.collection.distinct('x') <= how to get values from keys with specific id
+    names = mywlist.distinct('name')
+    return names
 
 
 
-#Get all items from watchlist
-def getList ():
-    print( type (rwlist))
-    return(rwlist)
 
-
-
-print(rwlist['name'])
-
-
-
-
-
-
-
-
-
-
-
-
-getList()
+# full_list()
+# get_names()
 
